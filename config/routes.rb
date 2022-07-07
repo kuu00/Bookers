e872fc5b481167c614a4739b3c3374317b040295
EC2 Default User <ec2-user@ip-172-31-26-113.ap-northeast-1.compute.internal>
2022-07-07 04:20:31 +0000

    [Update]3-課題

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   resources :books
-  post 'index' => 'index#create'
+  get 'books/:id' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
