817530175913353dd852e41e312ae1f0cb70d10e
EC2 Default User <ec2-user@ip-172-31-26-113.ap-northeast-1.compute.internal>
2022-07-17 14:08:44 +0000

    [Update]3-課題

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,20 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
-  
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/top'
+  end
+
   def index
   end
-  
+
   def show
   end
-  
+
   def edit
   end
 end
