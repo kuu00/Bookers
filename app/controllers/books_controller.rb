a140ec0009f0609793926cf7c2dcd2375b0049e8
EC2 Default User <ec2-user@ip-172-31-26-113.ap-northeast-1.compute.internal>
2022-07-28 14:40:29 +0000

    [Update]3-課題

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,8 +5,14 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    
+    if book.save
+      flash[:notice] = "Book was successfully created"
+      redirect_to book_path(book.id)
+    else
+      flash.now[:danger] = ""
+      render :new
+    end
   end
 
   def index
@@ -25,7 +31,14 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book.id)
+    
+    if book.save
+      flash[:notice] = "Book was successfully created"
+      redirect_to book_path(book.id)
+    else
+      flash.now[:danger] = ""
+      render :new
+    end
   end
   
   def destroy
